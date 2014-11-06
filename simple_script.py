ac62fe436d4835e784d2ac35f02790b276b775e8
eberlea <annikaleberle@gmail.com>
2014-11-06 12:23:33 -0800

    Turn into usable script with functions

--- simple_script.py
+++ simple_script.py
@@ -10,6 +10,20 @@ Created on Thu Nov  6 11:27:43 2014
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.random.gumbel(1,1,500000)
-plt.hist(data,100)
-plt.show()
+def show_bryant(data_length=500000, bins=100):
+    """ Create some data with the gumbel distribution, then plot as a historgram
+    Takes: 
+        data_length: how many points to generate
+        bins: how many bins to sort data into for histogram
+    Gives:
+        None 
+    """
+    data = np.random.gumbel(1,1,data_length)
+    plt.hist(data,bins)
+    plt.show()
+    
+def main():
+    show_bryant(10000,50)
+
+if __name__ == '__main__':
+    main()
