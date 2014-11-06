27efcb11b0779f05c331f44353fe91f466809fbe
eberlea <annikaleberle@gmail.com>
2014-11-06 11:38:53 -0800

    First iteration of simple script

---
+++ py_dave_6nov2014.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:15:01 2014
+
+@author: Annika
+"""
+
+import numpy as np 
+from matplotlib import pyplot as plt
+
+data = np.random.normal(1,1,10)
+
+plt.plot(data)
+
---
+++ simple_script.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+simple_script.py - generate some random data and then plot a histogram
+
+Created on Thu Nov  6 11:27:43 2014
+
+@author: Annika
+"""
+
